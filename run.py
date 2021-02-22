dd0eff4b713551a341ce1a53ec326f9721930a00
AnnaLouise500 <annamorrison500@hotmail.com>
2021-02-22 20:15:31 +0000

    removed all previous work and only working with html and python, base template and nav bar

--- run.py
+++ run.py
@@ -1,33 +1,76 @@
+import json
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
 
 
 app = Flask(__name__)
 
-
+# index
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
-@app.route("/search")
-def search():
-    return render_template("search.html")
+# create
+@app.route("/review")
+def review():
+    return render_template("review.html")
 
 
-@app.route("/create")
-def create():
-    return render_template("create.html")
-
-
+# add route
 @app.route("/addroute")
 def addroute():
     return render_template("addroute.html")
 
 
+# login
 @app.route("/login")
 def login():
     return render_template("login.html")
+
+
+# search
+@app.route("/search")
+def search():
+    return render_template("search.html")
+
+
+
+
+# @app.route("/##")
+# def about():
+#    data = []
+#    with open("data/company.json", "r") as json_data:
+#        data = json.load(json_data)
+#    return render_template("about.html", page_title="About", company=data)
+
+
+# @app.route("/about/<member_name>")
+# def about_member(member_name):
+#    member = {}
+#    with open("data/company.json", "r") as json_data:
+#        data = json.load(json_data)
+#        for obj in data:
+#            if obj["url"] == member_name:
+#                member = obj
+#    return render_template("member.html", member=member)
+
+
+# @app.route("/contact", methods=["GET", "POST"])
+# need to add this if want to do any forms within this HTML page
+# def contact():
+#    if request.method == "POST":
+#        flash("Thanks {}, we have received your message".format(
+#           request.form.get("name")))
+#    return render_template("contact.html", page_title="Contact")
+
+
+# @app.route("/careers")
+# def careers():
+#    return render_template("careers.html", page_title="Careers")
+# page type is a variable name
 
 
 if __name__ == "__main__":
@@ -35,3 +78,4 @@ if __name__ == "__main__":
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
         debug=True)
+# never use debug=true in a testing environment not in a submitted project

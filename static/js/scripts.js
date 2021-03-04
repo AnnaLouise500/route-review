f12a9ffa5f4e242412efa20915b962876325d02c
AnnaLouise500 <annamorrison500@hotmail.com>
2021-03-04 16:45:36 +0000

    added bootstrap tags input cdn files to routerecord page - they're still not linking properly though!

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,28 +1,21 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+// Range Slider
+function updateTextInput(val) {
+          document.getElementById('textInput').value=val; 
+        }
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+//location map
+function initMap() {
+        // The location of northwales
+        const northwales = { lat: 53.081996, lng: -3.659492 };
+        // The map, centered at northwales
+        const map = new google.maps.Map(document.getElementById("map"), {
+          zoom: 10,
+          center: northwales,
+        });
+        // The marker, positioned at northwales
+        const marker = new google.maps.Marker({
+          position: northwales,
+          map: map,
+        });
+      }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
